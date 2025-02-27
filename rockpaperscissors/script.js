9c04a6f670308ba22d78b7effbb2ebcd4d066d7f
abramepithey <abramepithey@gmail.com>
2025-02-27 09:13:49 -0700

    Remove pseudocode

--- rockpaperscissors/script.js
+++ rockpaperscissors/script.js
@@ -1,46 +1,3 @@
-/*
-
-function getComputerChoice() returns 'rock' 'paper' or 'scissors'
-    get random number - Math.random()
-    IF random number is less than 1/3 return 'rock'
-    ELSEIF random number is less than 2/3 return 'paper'
-    ELSE return 'scissors'
-
-function getHumanChoice() returns 'rock' 'paper' or 'scissors'
-    FOR 3 iterations
-        prompt for input, make it ToLower()
-        IF is valid input return input with first letter capitalized
-        ELSE alert bad input
-
-    alert too many invalid responses, using random answer
-    return getComputerChoice()
-
-function playRound(getComputerChoice(), getHumanChoice())
-    IF variables are equal alert 'Tie'
-    ELSEIF computerChoice == 'Rock' AND humanChoice == 'Scissors'
-    OR computerChoice == 'Paper' AND humanChoice == 'Rock'
-    OR computerChoice == 'Scissors' AND humanChoice == 'Paper'
-        alert 'You lose, {computerChoice} beats {humanChoice}'
-        increment computerScore
-    ELSE
-        alert 'You win, {computerChoice} beats {humanChoice}'
-        increment humanScore
-
-function playGame takes integer number of rounds, default 5
-    humanScore = 0
-    computerScore = 0
-
-    FOR parameter number of iterations
-        playRound()
-
-    IF humanScore == computerScore
-        alert 'Tie'
-    ELSEIF humanScore > computerScore
-        alert 'You win!'
-    ELSE
-        alert 'You lose...'
-
-*/
 let humanScore = 0;
 const humanScoreCounter = document.querySelector('.playerScore');
 let computerScore = 0;
@@ -78,20 +35,20 @@ function getHumanChoice(round, attempts = 3) {
     alert('Too many invalid responses, using random response');
     return getComputerChoice();
 }
-    
-    function playRound(computerChoice, humanChoice) {
-        if (computerChoice === humanChoice)
+
+function playRound(computerChoice, humanChoice) {
+    if (computerChoice === humanChoice)
         responseText.innerHTML = `Tie, go again!`
-        else if (computerChoice == 'Rock' && humanChoice == 'Scissors' ||
-                computerChoice == 'Paper' && humanChoice == 'Rock' ||
-                computerChoice == 'Scissors' && humanChoice == 'Paper') {
+    else if (computerChoice == 'Rock' && humanChoice == 'Scissors' ||
+            computerChoice == 'Paper' && humanChoice == 'Rock' ||
+            computerChoice == 'Scissors' && humanChoice == 'Paper') {
         responseText.innerHTML = `The computer gets a point, ${humanChoice} beats ${computerChoice}`
-            computerScore++;
-        }
-        else {
+        computerScore++;
+    }
+    else {
         responseText.innerHTML = `You get a point, ${humanChoice} beats ${computerChoice}`;
-            humanScore++;
-        }
+        humanScore++;
+    }
 
     humanScoreCounter.textContent = 'Your Score: ' + String(humanScore);
     computerScoreCounter.textContent = 'Computer Score: ' + String(computerScore);
